17ffdef65f50ab3047ad4a536af09730d2fc0c3c
Tim Povodnik <47430888+povodniktim@users.noreply.github.com>
2023-01-03 17:43:12 +0100

    Moved token to secrets.json and excluded it in .gitignore

--- Modules/Commands.cs
+++ Modules/Commands.cs
@@ -13,22 +13,17 @@ using Newtonsoft.Json;
 
 namespace DIscordBotTest.Modules
 {   
-    //You can also add separately the commands you want on the bot. To do this,in addition to the main program code, we create a new class that we will call 'Commands'. To add commands so that the bot responds to them, do as I did below.
     public class Commands : InteractiveBase
     {
         [Command("quiz", RunMode = RunMode.Async)]
         public async Task Quiz()
         {
-            // Initialize the score to 0
             int score = 0;
-            // Initialize the number of questions to 5
             int numberOfQuestions = 5;
-            // Initialize the current question number to 1
             int currentQuestionNumber = 1;
 
             for (int i = 0; i < numberOfQuestions; i++)
             {
-                // Create an HTTP client to send a request to the API
                 using (var client = new HttpClient())
                 {
                     // Send a request to the API to get 10 trivia questions in the category "Science: Computers" with medium difficulty
@@ -36,44 +31,33 @@ namespace DIscordBotTest.Modules
                         await client.GetAsync(
                             "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple");
 
-                    // Read the response as a JSON object
                     var json = await APIresponse.Content.ReadAsStringAsync();
                     dynamic data = JsonConvert.DeserializeObject(json);
 
-                    // Get the question and answer from the response
                     string question = data.results[0].question;
                     string correctAnswer = data.results[0].correct_answer;
 
-                    // Remove the "&quot;" symbol from the question using a regular expression
                     question = System.Text.RegularExpressions.Regex.Replace(question, "&quot;", "'");
                     question = System.Text.RegularExpressions.Regex.Replace(question, "&#039;", "'");
                     question = System.Text.RegularExpressions.Regex.Replace(question, "039", "'");
 
-                    // Get the incorrect answers from the response
                     List<string> incorrectAnswers = data.results[0].incorrect_answers.ToObject<List<string>>();
 
-                    // Add the correct answer to the list of incorrect answers
                     incorrectAnswers.Add(correctAnswer);
 
-                    // Shuffle the list of answers
                     incorrectAnswers = incorrectAnswers.OrderBy(x => Guid.NewGuid()).ToList();
 
-                    // Create the options
                     List<string> options = incorrectAnswers;
 
-                    // Create the embed message with the question and options
                     var embed = new EmbedBuilder()
                         .WithTitle($"(Question {currentQuestionNumber}/{numberOfQuestions})\n{question}")
                         .WithColor(new Color(0x00ddff))
                         .WithDescription(string.Join("\n", options.Select((o, i) => $"{EmojiForNumber(i + 1)} {o}\n")));
 
-                    // Send the question and options to the user
                     var questionMessage = await Context.Channel.SendMessageAsync(embed: embed.Build());
 
-                    // Create a task to wait for the user's response
                     var response = await NextMessageAsync();
 
-                    // Wait for either the response or the timer to complete
                     if (response != null)
                     {
                         // Check if the user's response was the option number
@@ -82,7 +66,6 @@ namespace DIscordBotTest.Modules
                             // Get the option corresponding to the number
                             string selectedOption = options[optionNumber - 1];
 
-                            // Check if the user's response was the correct answer
                             if (selectedOption.ToLower() == correctAnswer.ToLower())
                             {
                                 score++;
@@ -116,14 +99,13 @@ namespace DIscordBotTest.Modules
                     currentQuestionNumber++;
                 }
             }
-            // At the end of the game, create an embed message to display the score
+
             var scoreEmbed = new EmbedBuilder()
                 .WithTitle("Quiz Results")
                 .WithColor(new Color(0x00ddff))
                 .AddField("Score", $"{score}/{numberOfQuestions} :trophy:")
                 .Build();
 
-            // Send the embed message with the score to the user
             await ReplyAsync(embed: scoreEmbed);
         }
 
@@ -135,7 +117,6 @@ namespace DIscordBotTest.Modules
                 case 2: return new Emoji("2️⃣");
                 case 3: return new Emoji("3️⃣");
                 case 4: return new Emoji("4️⃣");
-                // Add more cases for more options
                 default: return new Emoji("❔");
             }
         }
--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Discord.Addons.Interactive;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace TutorialBot
 {
@@ -23,6 +25,7 @@ namespace TutorialBot
         private DiscordSocketClient _client;
         public CommandService _commands;
         private IServiceProvider _services;
+
 
         private SocketGuild guild;
 
@@ -42,13 +45,16 @@ namespace TutorialBot
                 .AddSingleton<InteractiveService>()
                 .BuildServiceProvider();
 
-            string token = "MTA1NTEzNjA1ODQwNjM0Njc2Mg.GKmPA7.NVBg2qtJwBTNgxMbgKRIy80NOVDAHA11HgD_PI";
+            string json = File.ReadAllText("secrets.json");
+            dynamic secrets = JsonConvert.DeserializeObject(json);
+
+            string discordToken = secrets.DISCORD_TOKEN;
 
             _client.Log += _client_Log;
 
             await RegisterCommandsAsync();
 
-            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.LoginAsync(TokenType.Bot, discordToken);
 
             await _client.StartAsync();
 
@@ -73,15 +79,15 @@ namespace TutorialBot
 
         // Read Input and get Output ( RECEIVE AND DO SOMETHING )
         public async Task HandleCommandAsync(SocketMessage arg)
-        {   
+        {
             var message = arg as SocketUserMessage;
             var context = new SocketCommandContext(_client, message);
-            var channel = _client.GetChannel(LogChannelID) as SocketTextChannel;         
+            var channel = _client.GetChannel(LogChannelID) as SocketTextChannel;
 
             //console log with message received and user info
             Console.WriteLine("-------------\nUser:  " + message.Author.Username + " with ID  " + message.Author.Id +
-                                                           "\nWrite:" +
-                                                           "\n" + message.ToString());
+                              "\nWrite:" +
+                              "\n" + message.ToString());
 
             //return (exit and do nothing) if author of message is the bot
             if (message.Author.IsBot) return;
@@ -93,71 +99,12 @@ namespace TutorialBot
             {
                 var result = await _commands.ExecuteAsync(context, argPos, _services);
                 if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
-                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition))
+                    await message.Channel.SendMessageAsync(result.ErrorReason);
             }
 
             //I usually transform text input in lower text ( A -> a ) to facilitate the reading of the text
             var text = message.ToString().ToLower();
-
-            //Create a new channel with the log from the bot
-            if (text == "/create log")
-            {
-                ulong messageChannelId = message.Channel.Id;
-                var textChannel = _client.GetChannel(messageChannelId) as SocketTextChannel;
-                guild = _client.GetGuild(textChannel.Category.GuildId);
-                var newChannel= await guild.CreateTextChannelAsync("log");
-
-                //SET ROLES OF THE CHANNEL LOG
-                //bot role (higher) - AllowAll (show and send messages) 
-                await newChannel.AddPermissionOverwriteAsync(guild.GetRole(967098609982124032), OverwritePermissions.AllowAll(newChannel));
-                //admins role - "modify" to set single actions role - in this case, to show channel but with sendMessages: PermValue.Deny (can't send messages)
-                await newChannel.AddPermissionOverwriteAsync(guild.GetRole(967417101558112381), OverwritePermissions.DenyAll(newChannel).Modify(viewChannel: PermValue.Allow, readMessageHistory: PermValue.Allow, sendMessages: PermValue.Deny));
-                //users role - users can't see the log channel - DENY ALL
-                await newChannel.AddPermissionOverwriteAsync(guild.EveryoneRole, OverwritePermissions.DenyAll(newChannel));
-
-                //log channel created - notify with text
-                LogChannelID = newChannel.Id;
-                await textChannel.SendMessageAsync("Channel created - Open it to see the Bot's log.");
-                await newChannel.SendMessageAsync("Channel created - Here you can see the Bot's log.");
-            }
-
-            //log massage in log channel with action of users and text
-            if (channel != null)
-             {
-                await channel.SendMessageAsync("-------------\nUser:  " + message.Author.Username + " with ID  " + message.Author.Id +
-                                                           "\nWrite:" +
-                                                           "\n" + message.ToString());
-             }
-
-            //read input and do something
-
-            //answer to the user
-            switch (text)
-            {
-                case "hi bot": 
-                    await message.Channel.SendMessageAsync("Hello "+ message.Author.Username+"!"); 
-                    break;
-                case "hello bot":
-                    await message.Channel.SendMessageAsync("Hello " + message.Author.Username + "!");
-                    break;
-            }
-            //block and delete words
-            if (text.Contains("bad word") || message.ToString().ToLower().Contains("badword"))
-            {
-                await message.Channel.SendMessageAsync("You can't say that things!\n\nMessage removed from the chat.");
-                await message.DeleteAsync();
-                //log in the log channel with the action
-                await channel.SendMessageAsync("ACTION: Bot removed the message for BAD WORDS");
-            }
-            
-           if (text.Contains("bot write me in private"))
-            {
-                //Private message to users
-                ulong userID = 0123456789;
-                var user = _client.GetUserAsync(userID).Result;
-                var channell = await user.CreateDMChannelAsync();
-                await channell.SendMessageAsync("what's up");
-            } 
         }
     }
 }
